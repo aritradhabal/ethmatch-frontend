7f4d90260808062569bbb15fd457dd23d3dba88f
aritradhabal <aritra@Aritras-MacBook-Air.local>
2025-09-28 10:16:27 +0530

    vercel depl

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const nextConfig: NextConfig = {
         "v5j7rrl9-3000.inc1.devtunnels.ms",
         "https://v5j7rrl9-3001.inc1.devtunnels.ms",
         "v5j7rrl9-3001.inc1.devtunnels.ms",
+        "https://ethglobal-backend-ekaxauyxxq-vp.a.run.app",
       ],
     },
   },
